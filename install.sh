0b7042bed0fc8b3221842d01aa7291799149f16b
e_t <77915530+erstan@users.noreply.github.com>
2021-08-20 09:46:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-sudo gcc src/quees.c -lm -o /usr/local/bin/quees
+
+gcc src/quees.c -lm -o /usr/local/bin/quees
+
+cp -f quees.1.gz /usr/local/share/man/man1
+mandb 
+
 echo "quees installation is completed"
 echo "run quees --help for usage information"
